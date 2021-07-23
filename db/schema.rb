d52efaaafd9b60bf25440d2e5cb45e37469ba8c1
yukihirasrr <rito.yukihira1111@gmail.com>
2021-07-23 21:57:28 +0900

    userモデルカラム追加許可、エラー修正

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_07_23_095821) do
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
-    t.string "birthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
