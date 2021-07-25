dc790da23ce9a367e26fede053c66e6a35a59a76
yukihirasrr <rito.yukihira1111@gmail.com>
2021-07-25 18:45:56 +0900

    バリデーションの設定、残りのActivHashクラスファイルの作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,16 @@ class Product < ApplicationRecord
   belongs_to_active_hash :oligation
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :date
+
+  with_options presence: true do
+    validates :p_name, :description, :image
+    
+    with_options numericality: { other_than: 1 } do
+      validates :category_id, :status_id, :oligation_id, :prefecture_id, :date_id
+    end
+
+    with_options format: { with: /\A[0-9]+\z/ } do
+      validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
+    end
+  end
 end
