3c1a3f9f394ed1c3980834de53adb19f298b6b6d
yukihirasrr <rito.yukihira1111@gmail.com>
2021-07-25 18:26:07 +0900

    ActiveHashクラスファイル作成、productモデルとの関連付け

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :oligation
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :date
 end
