c436ac61bbae0f5468df9f6c41cf354d523646a5
yukihirasrr <rito.yukihira1111@gmail.com>
2021-07-26 20:17:41 +0900

    商品情報編集ルーティング、コントローラー、ビュー作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :product_set, only: [edit, :update]
+
+
 
   def index
     @products = Product.all.order(created_at: "DESC")
@@ -19,7 +22,25 @@ class ProductsController < ApplicationController
       end
   end
 
+  def edit
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @product.update(item_params)
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
   def product_params
     params.require(:product).permit(:p_name, :description, :category_id, :status_id, :obligation_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
-    end
+  end
+
+  def product_set
+    @product = Product.find(params[:id])
+  end
 end
