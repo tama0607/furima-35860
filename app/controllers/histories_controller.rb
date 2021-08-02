cdafb5cbd391bc0db280d72d28a3f30b73999b1e
yukihirasrr <rito.yukihira1111@gmail.com>
2021-08-02 16:39:41 +0900

    aa

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -29,7 +29,7 @@ class HistoriesController < ApplicationController
   end
 
   def set_user
-    unless current_user.id == @product.user_id && @product.history == nil
+    unless current_user.id != @product.user_id && @product.history == nil
       redirect_to root_path
     end
   end
